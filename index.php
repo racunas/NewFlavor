c5050d78dd3e00b565e72da32fb896db845cb5fa
Ricardo Sandoval <racunas1500@alumno.ipn.mx>
2021-09-22 21:53:34 -0500

    INICIO DE PAGINA 1. Cloné un bloque la página mallchimp. Para recordar algunos puntos. 2. El segundo reto fue subirla a GitHub.

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+include "controlador/plantilla.controlador.php";
+
+$object = new controladorPlantilla(); 
+$object -> ctrObtenerPlantillaControlador();
+
+?>
---
+++ vista/modulos/aromatizante.html.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+            <img class="imgAro" src = "vista/asset/imagenes/aroma.webp">
+        </div>
+            
+        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+            <div class="introduccion">
+                <h2 class="introduccion__crear"> Crear mejor contenido </h2>
+                <p class="introduccion__descripcion my-4"> Haz que tu mara destaque con herramientas de diseño fáciles de usar y plantillas flexibles. Nuestro Asistente Creativo con inteligencia artificial(IA) generará diseños personalizados para ti en solo segundos.</p>
+                <a class="introduccion__enlace" href="#">Explora el gestor de contenidos</a>
+
+            </div>
+        </div>
+    </div>
+
+</div>
